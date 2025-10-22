0c4fd8d4c5bb844d29e81bc40864241fb0ae6d9b
brettmariani923@gmail.com <brettmariani923@gmail.com>
2025-10-22 01:23:17 -0500

    updated dbcontext

--- LibrarySystem.Data/Services/LibraryDbContext.cs
+++ LibrarySystem.Data/Services/LibraryDbContext.cs
@@ -27,12 +27,3 @@ public class LibraryDbContext : DbContext //acts as a bridge between c# objects 
     }
 }
 
-//for dbcontext to be able to do any work, it needs an instance of the DbContextOptions class, which contains configuration info like the database provider and connection string.
-// To handle this we are going to use dependency injection elsewhere in the application to provide the DbContextOptions when the LibraryDbContext is created.
-//because we want our dbcontextoptionos to work with our librarydbcontext, we use DbContextOptions<LibraryDbContext> as the type for the options parameter in the constructor,
-// This way, when the DI container creates an instance of LibraryDbContext, it knows to provide the correct options specifically configured for it.
-
-//options serves as the name of the parameter that will hold the configuration settings needed by the DbContext.
-//because we want to pass those options to the base DbContext class, we call the base constructor with : base(options)
-
-//By doing this, we ensure that the LibraryDbContext is properly configured with the necessary settings to connect to the database and perform its operations.
